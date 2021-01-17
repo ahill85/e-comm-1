1cc1ea647ab52b07a2bfed8888859f10c25d461a
Andrew Hill <adhill6@gmail.com>
2021-01-18 10:48:36 +1100

    created react project to learn about react nd crate a ecomm site

---
+++ src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='menu-item-content'>
+          <h1 className='menu-item-title'>Hats</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='menu-item-content'>
+          <h1 className='menu-item-title'>Jackets</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='menu-item-content'>
+          <h1 className='menu-item-title'>Sneakers</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='menu-item-content'>
+          <h1 className='menu-item-title'>Womens</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+        <div className='menu-item-content'>
+          <h1 className='menu-item-title'>Mens</h1>
+          <span className='menu-item-subtitle'>Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
